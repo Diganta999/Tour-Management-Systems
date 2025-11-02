acf3263c79605892e83f65467726bbd352a651a7
Diganta999 <digantasikder123@gmail.com>
2025-11-02 13:13:12 +0600

    add bcryptjs for password hashing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,30 +2,34 @@ import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes"
+import bcryptjs from "bcryptjs"
 
 const createUserService = async (payload: Partial<IUser>) => {
 
-    const {email,...rest}=payload;
-    const isUserExist = await User.findOne({email});
-    if(isUserExist){
-        throw  new AppError(httpStatus.BAD_REQUEST,"user is all ready exist")
+    const { email, password, ...rest } = payload;
+    const isUserExist = await User.findOne({ email });
+    if (isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "user is all ready exist")
     }
-    const auths:IAuthProvider ={provider:"credential",providerId:email as string} 
-const user = await User.create({
-    email,
-    auths,
-    ...rest
-})
- return user
+    const inCryptPassword = await bcryptjs.hash(password as string, 10);
+    
+    const auths: IAuthProvider = { provider: "credential", providerId: email as string }
+    const user = await User.create({
+        email,
+        auths,
+        password: inCryptPassword,
+        ...rest
+    })
+    return user ;
 }
 
-const getAllUsersService=async()=>{
+const getAllUsersService = async () => {
     const users = await User.find({})
-    const total= await User.countDocuments()
+    const total = await User.countDocuments()
     return {
-        data:users,
-        meta:{
-            total:total
+        data: users,
+        meta: {
+            total: total
         }
     }
 }
