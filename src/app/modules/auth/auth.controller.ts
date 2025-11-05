72afd18c8c6975fddf3c44b3b855fa9336b6d84f
Diganta999 <digantasikder123@gmail.com>
2025-11-05 20:42:19 +0600

    optimize AuthController and cookie-based JWT handling|

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,17 +5,12 @@ import { AuthService } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
 import statusCode from "http-status-codes"
 import AppError from "../../errorHelpers/AppError";
+import { setAuthCookie } from "../../utils/setCookie";
 
 const authLoginController = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
     const user = await AuthService.authLoginService(req.body)
-    res.cookie("accessToken",user.accessToken,{
-        httpOnly:true,
-        secure:false
-    })
-    res.cookie("refreshToken",user.refreshToken,{
-        httpOnly:true,
-        secure:false
-    })
+    
+    setAuthCookie(res,user)
     
     sendResponse(res,{
         statusCode:statusCode.OK,
@@ -30,11 +25,12 @@ const getNewAccessToken = catchAsync(async(req:Request,res:Response,next:NextFun
         throw new AppError(statusCode.BAD_REQUEST,"no refresh token received from cookies !!!!!!!!")
     }
     const tokenInfo = await AuthService.getNewAccessToken(refreshToken as string)
+    setAuthCookie(res,tokenInfo)
     sendResponse(res,{
         statusCode:statusCode.OK, 
         message:"token generate successfully",
         success:true,
-        data:tokenInfo.accessToken
+        data:tokenInfo
     })
 })
 
